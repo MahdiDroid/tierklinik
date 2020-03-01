c75e8bae0abe329075eb4a3fada7830550b953b7
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-03-01 17:34:18 +0300

    Initial project add

---
+++ src/main/java/com/mahdidroid/tierklinik/TierklinikApplication.java
@@ -0,0 +1,13 @@
+package com.mahdidroid.tierklinik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierklinikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TierklinikApplication.class, args);
+    }
+
+}
