755d504b7906364d24f7af88f4eb9ba8f0a7ce84
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-03-03 14:04:20 +0300

    iml map based services . Closes #6

---
+++ TierKlinik-data/src/main/java/com/MahdiDroid/tierklinik/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.MahdiDroid.tierklinik.services.map;
+
+import com.MahdiDroid.tierklinik.model.Owner;
+import com.MahdiDroid.tierklinik.services.CrudService;
+import com.sun.xml.bind.v2.model.core.ID;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements CrudService<Owner,Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ TierKlinik-data/src/main/java/com/MahdiDroid/tierklinik/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.MahdiDroid.tierklinik.services.map;
+
+import com.MahdiDroid.tierklinik.model.Owner;
+import com.MahdiDroid.tierklinik.model.Pet;
+import com.MahdiDroid.tierklinik.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
